5cbb8fd40192b099f46c257f5c809af26d066110
Joel <tiago7carapinha@gmail.com>
2025-03-03 17:38:55 +0000

    Initial State of Footdle

---
+++ Footdle/Footdle.js
@@ -0,0 +1,193 @@
+import {SetUpAutoComplete} from "../Utilities/autocomplete.js";
+import {loadAndParseCsv, ReturnRandomRow, ReturnRow} from "../Utilities/queryCsv.js";
+
+let chosenPlayer = null
+let ArrayOfPlayers = null
+
+class Player
+{
+    constructor()
+    {
+        this.name = "name";
+        this.nation = "nation";
+        this.positions = ["position" , "position"];
+        this.squad = "squad";
+        this.age = 1;
+        this.goals = 1;
+    }
+
+    PopulatePlayer(array)
+    {
+        this.name = array["Player"];
+        this.nation = array["nation"];
+        this.positions = array["position"];
+        this.squad = array["squad"];
+        this.age = array["age"];
+        this.goals = array["goals"];
+    }
+
+    ComparePlayers(Otherplayer)
+    {
+
+        let nameResult = this.CompareStrings(this.name, Otherplayer.name);
+
+        let nationResult = this.CompareStrings(this.name, Otherplayer.name);
+
+        let positionResult = this.CompareArrayOfStrings(this.positions, Otherplayer.positions);
+
+        let squadResult = this.CompareStrings(this.name, Otherplayer.name);
+
+        let ageResult = this.CompareNumbers(this.age, Otherplayer.age);
+
+        let goalsResult= this.CompareNumbers(this.goals, Otherplayer.goals);
+
+    }
+
+    CompareNumbers(thisNumber, theirNumber)
+    {
+        /* the player to figure out is the this*/
+        if ( thisNumber === theirNumber ) return "equal";
+
+        if (thisNumber > theirNumber) return "bigger";
+
+        if (thisNumber < theirNumber) return "smaller";
+    }
+
+    CompareStrings(thisString, theirString)
+    {
+        return thisString === theirString;
+    }
+
+    CompareArrayOfStrings(thisArray, theirArray)
+    {
+        let booleanResults = {};
+        let currentResult = false;
+        let overlap = "none"
+
+        thisArray.forEach((thisElement) =>
+        {
+            if (thisElement == null) return;
+
+            currentResult = false;
+
+            theirArray.forEach((theirElement) =>
+            {
+                if (theirElement == null) return;
+
+                if (thisElement === theirElement)
+                {
+                    currentResult = true;
+                }
+            })
+
+            booleanResults.push(currentResult);
+        })
+
+
+        let values = Object.values(booleanResults);
+        let allTrue = values.every(val => val === true);
+        let allFalse = values.every(val => val === false);
+
+        if (allTrue)
+        {
+            overlap =  "full";
+        }
+        else if (allFalse)
+        {
+            overlap = "none";
+        }
+        else
+        {
+            overlap = "some";
+        }
+
+        return overlap;
+    }
+
+
+}
+
+export async function SettingUpPage()
+{
+    //get the csvpath for the players
+
+    //get all the players for the autocomplete
+    await SetUpAutoComplete(["../csv/PremierLeague_2022_2023_Players.csv"], "Player", "input-box", "result-box")
+}
+
+export function GetPlayer(name)
+{
+    //todo make a class for getting the players info ant etc
+    let PlayerObject = ReturnRow(ArrayOfPlayers, "Player", name)
+
+    if (PlayerObject === undefined || PlayerObject === null)
+    {
+        console.error("there isn't a player with the name of " + name)
+        return null;
+    }
+
+    return GetPlayerFromPlayerObject(PlayerObject);
+}
+
+export function GetRandomPlayer()
+{
+    let PlayerObject = ReturnRandomRow(ArrayOfPlayers);
+
+    if (PlayerObject === undefined || PlayerObject === null)
+    {
+        console.error("PlayerRow was not found");
+        return null;
+    }
+
+    return GetPlayerFromPlayerObject(PlayerObject);
+}
+
+export function SelectNewChosenPlayer()
+{
+    chosenPlayer = GetRandomPlayer();
+}
+
+export function GetPlayerFromPlayerObject(PlayerRow)
+{
+    let player = new Player();
+    player.PopulatePlayer(PlayerRow);
+
+    return player;
+}
+
+export function MakeGuess()
+{
+    let inputBox = document.getElementById("input-box")
+    if (inputBox == null) return;
+
+    if (inputBox.value.trim() === chosenPlayer.name)
+    {
+        alert("Correct")
+    }
+    else
+    {
+        alert("Incorrect")
+    }
+
+
+}
+
+$(document).ready(async function () {
+
+    ArrayOfPlayers = await loadAndParseCsv("../csv/PremierLeague_2022_2023_Players.csv")
+
+    await SettingUpPage();
+
+    await SelectNewChosenPlayer()
+
+    console.log(chosenPlayer.name);
+
+    let submitButton = document.getElementById("submit-btn")
+    if (submitButton != null ) submitButton.onclick = MakeGuess;
+
+    //wait for a response from the user
+
+    //compare the two hidden player vs the selected player
+    //create a new row with the selected player
+    //each information will be compared to chosen player
+})
